0e7213015e9085eb79d65befab40166775128b94
leico <yoshitaka_12@iamas.ac.jp>
2015-03-27 13:56:35 +0900

    data = json

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 $(function(){
-  var url = window.location.href;
-  url = url.split('/');
-  var page = url[url.length - 1].split('.')[0];
-  alert(page);
+  
+  var page = ((function(){
+               var url = window.location.href;
+               url = url.split('/');
+               return url[url.length - 1].split('.')[0];
+               })());
+  
+  var data;
+  
+  $.getJSON('./' + page + '.json', function(json){
+            data = json;
+            
+            });
+  
 });
